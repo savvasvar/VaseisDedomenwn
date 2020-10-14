93f30b693e886aaf577269be8962d2953464234a
atak96thanasis@gmail.com <AtaKon>
2020-10-14 16:54:06 +0300

    Implemented sql connection and first testing queries.

--- Documents/NetBeansProjects/eshop/src/main/java/com/mycompany/eshop/main.java
+++ Documents/NetBeansProjects/eshop/src/main/java/com/mycompany/eshop/main.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.eshop;
 
+import java.sql.*;
+
 /**
  *
  * @author Thanasis
@@ -14,8 +16,30 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        System.out.println("Test");
+    static String     driverClassName = "org.postgresql.Driver" ;
+    static String     url = "jdbc:postgresql://dblabs.it.teithe.gr:5432/it154474" ;
+    static Connection dbConnection = null;
+    static String     username = "it154474";
+    static String     passwd = "test123";
+    static Statement statement=null;
+    static ResultSet rs =null;
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName (driverClassName);
+        dbConnection = DriverManager.getConnection (url, username, passwd);
+        statement = dbConnection.createStatement();
+        
+        String selectString = "select * from roles";
+        rs=statement.executeQuery(selectString);
+        while(rs.next()){
+            String role_name=rs.getString("role_name");
+            int role_id=rs.getInt("role_id");
+            System.out.println(role_id+" "+role_name);
+        }
+        statement.close();
+        
+        
+        dbConnection.close();
     }
     
 }
